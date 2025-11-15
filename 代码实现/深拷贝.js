342a51d27845d18fe2235f0282b1a6f1281d12bb
nmsn <136696700@qq.com>
2025-11-15 15:59:15 +0800

    feat: 更新深拷贝方法

--- 代码实现/深拷贝.js
+++ 代码实现/深拷贝.js
@@ -1,3 +1,76 @@
+/**
+ * 深拷贝有两个需要注意的点
+ * 1. 循环引用问题（通过 weakmap 解决）
+ * 2. 递归爆栈问题（通常不会遇到，如果遇到，通过递归该循环来进行优化）
+ */
+
+// 辅助函数
+function isObject(x) {
+    return Object.prototype.toString.call(x) === '[object Object]';
+}
+
+// 简易版深拷贝
+function cloneDeep(source, hash = new WeakMap()) {
+  if (!isObject(source)) return source
+  if (hash.has(source)) return hash.get(source)
+
+  var target = Array.isArray(source) ? [] : {}
+  hash.set(source, target)
+
+  for (var key in source) {
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      if (isObject(source[key])) {
+        target[key] = cloneDeep(source[key], hash)
+      } else {
+        target[key] = source[key]
+      }
+    }
+  }
+  return target
+}
+
+// 处理爆栈版（了解即可）
+function cloneDeep(x) {
+  const root = {}
+
+  const loopList = [
+    {
+      parent: root,
+      key: undefined,
+      data: x,
+    },
+  ]
+
+  while (loopList.length) {
+    const node = loopList.pop()
+    const parent = node.parent
+    const key = node.key
+    const data = node.data
+
+    let res = parent
+    if (typeof key !== 'undefined') {
+      res = parent[key] = {}
+    }
+
+    for (let k in data) {
+      if (data.hasOwnProperty(k)) {
+        if (typeof data[k] === 'object') {
+          loopList.push({
+            parent: res,
+            key: k,
+            data: data[k],
+          })
+        } else {
+          res[k] = data[k]
+        }
+      }
+    }
+  }
+
+  return root
+}
+
+// 完全版深拷贝
 const completeDeepClone = (target, map = new WeakMap()) => {
   // 基本数据类型，直接返回
   if (typeof target !== "object" || target === null) return target;
@@ -40,6 +113,7 @@ const completeDeepClone = (target, map = new WeakMap()) => {
   // 复制所有可枚举属性
   for (let prop in target) {
     if (target.hasOwnProperty(prop)) {
+      // 此处就是递归调用时，可能出现爆栈
       cloneTarget[prop] = completeDeepClone(target[prop], map);
     }
   }
