a0963c1a637ec0737c200ff47677665889504a87
nmsn <136696700@qq.com>
2025-11-18 22:39:26 +0800

    feat: 输出题

---
+++ 代码输出/变量提升.js
@@ -0,0 +1,6 @@
+var func = 1
+function func() {}
+console.log(func + func)
+
+// 2
+// var 和 function 存在变量提升，function 的变量提升优先级更高
---
+++ 代码输出/引用类型.js
@@ -0,0 +1,7 @@
+let obj1 = { x: 1 }
+let obj2 = obj1
+obj2.y = 2
+obj2 = { y: 20 }
+console.log('obj1', obj1)
+
+// obj1 { x: 1, y: 2 }
